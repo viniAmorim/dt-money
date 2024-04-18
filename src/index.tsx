b8dde81999dad6440ce4cc51160da38a111d1416
viniAmorim <amorimm.viniciuss@gmail.com>
2024-04-18 18:59:10 -0300

    list transactions and seeds

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,52 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import { Model, createServer } from "miragejs";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
 import { App } from "./App";
 
 createServer({
   models: {
     transaction: Model,
   },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freela webpage",
+          type: "deposit",
+          category: "Dev",
+          amount: 4500,
+          createdAt: new Date("2021-03-15 08:00:00"),
+        },
+
+        {
+          id: 2,
+          title: "Supermercado",
+          type: "withdraw",
+          category: "Compras",
+          amount: 1200,
+          createdAt: new Date("2021-03-15 13:00:00"),
+        },
+      ],
+    });
+  },
   routes() {
     this.namespace = "api";
 
     this.get("/transactions", () => {
-      return this.schema.all("transactions");
+      return this.schema.all("transaction");
     });
 
     this.post("/transactions", (schema, request) => {
       const data = JSON.parse(request.requestBody);
-
       return schema.create("transaction", data);
     });
   },
 });
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
